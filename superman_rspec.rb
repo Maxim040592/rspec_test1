e8ba8f647309f5c4b05d2528ef134b52e709e8fe
Maxim040592 <maxim_007_92@mail.ru>
2021-01-07 19:45:18 +0500

    correct_code_2

--- superman_rspec.rb
+++ superman_rspec.rb
@@ -2,24 +2,23 @@ require './superman'
 
 describe Superman do 
   
+  before do
+     @superman = Superman.new 'mike'
+  end
 
   it "sumerman is name capitalize" do
-    superman = Superman.new 'mike'
-    expect(superman.name).to eq 'Mike' # аналог sumerman.name == Mike
+    expect(@superman.name).to eq 'Mike' # аналог sumerman.name == Mike
   end
   
   it "sumerman is hp power_up" do
-    superman = Superman.new 'mike'
-    expect(superman.power_up).to eq 110 
+    expect(@superman.power_up).to eq 110 
   end
 
   it "sumerman is hp power_down" do
-    superman = Superman.new 'mike'
-    expect(superman.power_down).to eq 90 
+    expect(@superman.power_down).to eq 90 
   end
   it "sumerman is all_infa" do
-    superman = Superman.new 'mike'
-    expect(superman.infa_all).to eq "Mike has a hp for 100"
+    expect(@superman.infa_all).to eq "Mike has a hp for 100"
   end
 
 end
